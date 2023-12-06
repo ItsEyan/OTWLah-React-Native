65f600c6a3e3192d6aaf8ca373e20069e8e0425d
ItsEyan <EyanLee83@gmail.com>
2023-12-06 21:39:25 +0800

    added all set details functionality

--- Client/screens/appScreens/Map.js
+++ Client/screens/appScreens/Map.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 import * as Location from 'expo-location';
 import React, { useEffect, useRef, useState } from 'react';
 import {
+	Dimensions,
 	Keyboard,
 	SafeAreaView,
 	StatusBar,
 	StyleSheet,
 	Text,
+	TouchableOpacity,
 	View,
 } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
@@ -16,6 +18,9 @@ import MapView, {
 	MarkerAnimated,
 	Polyline,
 } from 'react-native-maps';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import Button from '../../components/Button';
 import CurrentLocationIcon from '../../components/CurrentLocationIcon';
 import ActionButton from '../../components/FloatingActionButton';
 import FloatingActionMenu from '../../components/FloatingActionMenu';
@@ -37,8 +42,43 @@ const Map = () => {
 	const [currentPlace, setCurrentPlace] = useState(null);
 	const [userLocation, setUserLocation] = useState(null);
 	const [routeCoords, setRouteCoords] = useState(null);
+	const [step, setStep] = useState('selecting');
+	const [canPanelDrag, setCanPanelDrag] = useState(true);
+
+	// date picker
+	const [arrivalTime, setArrivalTime] = useState(new Date());
+	const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+	const showDatePicker = () => {
+		setCanPanelDrag(false);
+		setDatePickerVisibility(true);
+	};
+
+	const hideDatePicker = () => {
+		setCanPanelDrag(true);
+		setDatePickerVisibility(false);
+	};
+
+	const options = {
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	};
+
+	const handleConfirm = (date) => {
+		setArrivalTime(date);
+		hideDatePicker();
+	};
+
+	// panel
 
 	const isAndroid = Platform.OS == 'android';
+	const screenHeight = Dimensions.get('window').height;
+
+	const [panelMaxHeight, setPanelMaxHeight] = useState(screenHeight / 3.5);
+	const [panelMinHeight, setPanelMinHeight] = useState(0);
 
 	useEffect(() => {
 		(async () => {
@@ -56,6 +96,39 @@ const Map = () => {
 			moveToLocation(location?.coords?.latitude, location?.coords?.longitude);
 		})();
 	}, []);
+
+	const findZoomCoords = (coords) => {
+		let minLat, maxLat, minLng, maxLng;
+
+		// init values
+		((coord) => {
+			minLat = coord.latitude;
+			maxLat = coord.latitude;
+			minLng = coord.longitude;
+			maxLng = coord.longitude;
+		})(coords[0]);
+
+		// calculate values
+		coords.map((coord, index) => {
+			minLat = Math.min(minLat, coord.latitude);
+			maxLat = Math.max(maxLat, coord.latitude);
+			minLng = Math.min(minLng, coord.longitude);
+			maxLng = Math.max(maxLng, coord.longitude);
+		});
+
+		const midLat = (minLat + maxLat) / 2;
+		const midLng = (minLng + maxLng) / 2;
+
+		const deltaLat = maxLat - minLat + 0.01;
+		const deltaLng = maxLng - minLng + 0.01;
+
+		return {
+			latitude: midLat,
+			latitudeDelta: deltaLat,
+			longitude: midLng,
+			longitudeDelta: deltaLng,
+		};
+	};
 
 	const moveToLocation = (lat, long) => {
 		mapRef.current.animateToRegion(
@@ -121,11 +194,105 @@ const Map = () => {
 						longitude: destinationLoc.lng,
 					});
 					setRouteCoords(coords);
+					mapRef.current.animateToRegion(findZoomCoords(coords), 1000);
 				});
 		} catch (error) {
 			console.log(error);
 		}
 	};
+
+	const panelHidden = (value) => {
+		if (value === 0 && currentPlace && step !== 'travelling') {
+			setStep('selecting');
+			setArrivalTime(new Date());
+			setCurrentPlace(null);
+			setPanelMaxHeight(screenHeight / 3.5);
+		}
+	};
+
+	const locationPanelContent = () => {
+		return (
+			<View style={styles.panelContainer}>
+				<Text style={styles.locatioName}>{currentPlace?.name}</Text>
+				<Text style={styles.locationAddress}>
+					{currentPlace?.formatted_address}
+				</Text>
+				<View style={styles.panelSeparator} />
+				<View style={{ marginTop: 30, marginHorizontal: 20 }}>
+					<Button
+						title="Use This Location"
+						filled={true}
+						color={COLORS.primary}
+						onPress={() => {
+							setStep('settime');
+							setPanelMaxHeight(screenHeight / 2.3);
+							this._panel.show(screenHeight / 2.3);
+						}}
+					/>
+				</View>
+			</View>
+		);
+	};
+
+	const setTimePanelContent = () => {
+		return (
+			<View style={styles.panelContainer}>
+				<Text style={styles.locatioName}>{currentPlace?.name}</Text>
+				<Text style={styles.locationAddress}>
+					{currentPlace?.formatted_address}
+				</Text>
+				<View style={styles.panelSeparator} />
+				<Text
+					style={[styles.locatioName, { paddingTop: 30, textAlign: 'center' }]}>
+					Arrival Time
+				</Text>
+				<View
+					style={{
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<View
+						style={{
+							borderWidth: 2,
+							borderRadius: 10,
+							paddingHorizontal: 20,
+						}}>
+						<TouchableOpacity onPress={showDatePicker}>
+							<Text style={styles.arrivalTime}>
+								{arrivalTime.toLocaleDateString('en-US', options)}
+							</Text>
+						</TouchableOpacity>
+						<DateTimePickerModal
+							date={arrivalTime}
+							isVisible={isDatePickerVisible}
+							mode="datetime"
+							onConfirm={handleConfirm}
+							onCancel={hideDatePicker}
+							buttonTextColorIOS={COLORS.black}
+							textColor={COLORS.black}
+							minimumDate={new Date()}
+						/>
+					</View>
+				</View>
+				<View style={{ marginTop: 30, marginHorizontal: 20 }}>
+					<Button
+						title="Submit"
+						filled={true}
+						color={COLORS.primary}
+						onPress={() => {
+							setStep('travelling');
+							getRoute(userLocation, currentPlace?.geometry?.location);
+							setPanelMinHeight(screenHeight / 4);
+							setPanelMaxHeight(screenHeight / 1.5);
+							this._panel.show(screenHeight / 4);
+						}}
+					/>
+				</View>
+			</View>
+		);
+	};
+
+	const setTravellingPanelContent = () => {};
 
 	return (
 		<View style={styles.container}>
@@ -134,6 +301,7 @@ const Map = () => {
 				onPress={() => Keyboard.dismiss()}
 				ref={mapRef}
 				style={styles.map}
+				showsCompass={false}
 				initialRegion={{
 					latitude: 1.29027,
 					latitudeDelta: latDelta,
@@ -189,7 +357,7 @@ const Map = () => {
 							details?.geometry?.location?.lat,
 							details?.geometry?.location?.lng
 						);
-						getRoute(userLocation, details?.geometry?.location);
+						this._panel.show();
 					}}
 					query={{
 						key: GOOGLE_IOS_API_KEY,
@@ -212,6 +380,22 @@ const Map = () => {
 				</View>
 			</SafeAreaView>
 			<FloatingActionMenu />
+			<SlidingUpPanel
+				ref={(c) => (this._panel = c)}
+				draggableRange={{ top: panelMaxHeight, bottom: panelMinHeight }}
+				snappingPoints={[panelMinHeight, panelMaxHeight]}
+				backdropOpacity={0}
+				onMomentumDragEnd={(value) => panelHidden(value)}
+				allowDragging={canPanelDrag}>
+				<View style={styles.panel}>
+					<View style={{ alignItems: 'center' }}>
+						<View style={styles.panelHandle} />
+					</View>
+					{step === 'selecting' && locationPanelContent()}
+					{step === 'settime' && setTimePanelContent()}
+					{step === 'travelling' && setTravellingPanelContent()}
+				</View>
+			</SlidingUpPanel>
 		</View>
 	);
 };
@@ -234,6 +418,48 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: COLORS.white,
 		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+	},
+	panel: {
+		flex: 1,
+		backgroundColor: 'white',
+		justifyContent: 'flex-start',
+		borderRadius: 16,
+	},
+	panelHandle: {
+		width: 150,
+		height: 6,
+		borderRadius: 10,
+		backgroundColor: COLORS.grey,
+		marginTop: 10,
+		opacity: 0.6,
+	},
+	panelSeparator: {
+		height: 1,
+		backgroundColor: COLORS.grey,
+		marginTop: 10,
+		opacity: 0.6,
+	},
+	locatioName: {
+		fontSize: 25,
+		paddingBottom: 10,
+		fontWeight: 'bold',
+		color: COLORS.black,
+		textAlign: 'left',
+	},
+	locationAddress: {
+		fontSize: 15,
+		color: COLORS.black,
+		textAlign: 'left',
+	},
+	panelContainer: {
+		marginHorizontal: 20,
+		marginTop: 20,
+	},
+	arrivalTime: {
+		fontSize: 20,
+		color: COLORS.black,
+		textAlign: 'center',
+		paddingVertical: 10,
 	},
 });
 
